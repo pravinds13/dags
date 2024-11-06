498319b7e7da4859456716e7049bfb03029fbd0b
pravinds13 <pravin.lr@gmail.com>
2024-11-06 13:44:29 -0500

    Update dataproc_with_deferable.py

--- dags/dataproc_with_deferable.py
+++ dags/dataproc_with_deferable.py
@@ -28,7 +28,6 @@ with DAG('dataproc_deferrable_example',
         batch_id='example-batch',
         deferrable=True,
         result_retry=Retry(maximum=100.0, initial=10.0, multiplier=1.0),
-        num_retries_if_resource_is_not_ready=3,
     )
 
     get_batch = DataprocGetBatchOperator(
