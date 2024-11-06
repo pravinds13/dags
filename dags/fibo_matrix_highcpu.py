25b9c1b27466f414ec3dd54563278a87378f609b
pravinds13 <pravin.lr@gmail.com>
2024-11-06 12:45:07 -0500

    Update fibo_matrix_highcpu.py

--- dags/fibo_matrix_highcpu.py
+++ dags/fibo_matrix_highcpu.py
@@ -25,7 +25,7 @@ def matrix_multiplication():
 default_args = {
     'owner': 'airflow',
     'depends_on_past': False,
-    'start_date': datetime(2024, 11, 4),
+    'start_date': datetime(2024, 11, 5),
     'email_on_failure': False,
     'email_on_retry': False,
     'retries': 1,
