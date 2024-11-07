4c8651bdef8094bc38a84f90fe35007d83e79f83
pravinds13 <pravin.lr@gmail.com>
2024-11-07 13:16:26 -0500

    Create bigquery_without_deferable.py

---
+++ dags/bigquery_without_deferable.py
@@ -0,0 +1,26 @@
+from airflow import DAG
+from airflow.providers.google.cloud.operators.bigquery import BigQueryInsertJobOperator
+from datetime import datetime
+
+default_args = {
+    'start_date': datetime(2024, 11, 7),
+}
+
+with DAG('bigquery_example',
+         default_args=default_args,
+         schedule_interval='@once',
+         catchup=False) as dag:
+
+    create_table = BigQueryInsertJobOperator(
+        task_id='create_table',
+        configuration={
+            "query": {
+                "query": """
+                    CREATE TABLE `ford-743794c14d5ab9bafaac1a08.dag_test_dataset.public_data_test` AS
+                    SELECT * FROM `bigquery-public-data.github_repos.contents`
+                """,
+                "useLegacySql": False,
+            }
+        },
+        location='US'
+    )
